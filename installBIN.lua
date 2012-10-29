37549463b234e477362861d6ff0cffe2a633311d
Ariel Lothlorien <git@arielkauai.com>
2012-10-29 10:52:50 -1000

    Fixed installBIN.lua
    Signed-off-by: Ariel Lothlorien <git@arielkauai.com>

--- installBIN.lua
+++ installBIN.lua
@@ -68,12 +68,12 @@ end
 for _,file in ipairs( files ) do
 	if string.sub(file, -4, -1) == ".lua" then
 		write("Copy File: " .. "/bin" .. string.sub(file, 1, -5))
-		os.delete("/bin" .. string.sub(file, 1, -5))
-		os.copy("/disk/bin" .. string.sub(file, 1, -5), "/bin" .. string.sub(file, 1, -5))
+		fs.delete(string.sub(file, 1, -5))
+		fs.copy("/disk/bin" .. file, "/bin" .. string.sub(file, 1, -5))
 	else
 		write("Copy File: " .. "/bin" .. file)
-		os.delete("/bin" .. file)
-		os.copy("/disk/bin" .. file, "/bin" .. string.sub(file, 1, -5))
+		fs.delete("/bin" .. file)
+		fs.copy("/disk/bin" .. file, "/bin" .. string.sub(file, 1, -5))
 	end
 	os.sleep( 0.1 )
 	write(".")
@@ -85,7 +85,6 @@ for _,file in ipairs( files ) do
 	write("\n")
 end
 
-string.sub("123456789", -4, -1)
 --[[
 for dir in dirs do
 	write("Creating lib/"+dir)
