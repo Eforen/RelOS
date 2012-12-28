7c93f82a50d5577fc0dfa69d24e12cdc822f66c4
Ariel Lothlorien <github@arielkauai.com>
2012-12-28 11:20:05 -1000

    Fixed satellite/buildTest.lua and moved it to satellite/build.lua
    Signed-off-by: Ariel Lothlorien <github@arielkauai.com>

--- bin/satellite/build.lua
+++ bin/satellite/build.lua
@@ -1,218 +1,344 @@
-local tArgs = { ... }
-if #tArgs < 4 then
-  print("Usage : satelite <x> <y> <z> <f>")
-  return
-end
-local Xpos = tonumber(tArgs[1])
-local Ypos = tonumber(tArgs[2])
-local Zpos = tonumber(tArgs[3])
-local facing = tonumber(tArgs[4])
+sm = smartmove
+t = turtle
 
-function buildUp(floppySlot)
-  -- Put the disk drive
-  turtle.select(3)
-  turtle.place()
-  -- Load disk into drive and write startup file
-  turtle.select(floppySlot)
-  turtle.drop()
-  file = fs.open("disk/startup","w")
-  file.writeLine("shell.run(\"gps\",\"host\","..Xpos..","..Ypos..","..Zpos..")")
-  file.close()
-  -- put the computer
-  turtle.up()
-  turtle.select(2)
-  turtle.place()
-  -- place the modem
-  turtle.up()
-  turtle.up()
-  turtle.forward()
-  turtle.select(4)
-  turtle.placeDown()
-  turtle.back()
-  turtle.down()
-  turtle.down()
-  -- turn computer on
-  computer = peripheral.wrap("front")
-  sleep(0.5)
-  if computer then
-    computer.turnOn()
-  end
-  turtle.down()
+function returnToDock()
+	sm.moveTo(352,64,458,0)
 end
 
-if type(Xpos) ~= "number" then
-  print("x must be number")
-  return
+local headerText = "***************************************\n**   Welcome to Satellite builder    **\n**   v1.0                            **\n***************************************\n"
+
+function header()
+	common.printStandardHeader(headerText)
+	--term.clear()
+	--print(headerText)
 end
-if type(Ypos) ~= "number" then
-  print("y must be number")
-  return
+
+-- Setup Vars
+local kill = false
+local waitingConfermation = false
+
+--Enter Item selectiong phase
+local slotFuel = 1
+local slotComputer = 2
+local slotDiskDrive = 3
+local slotModem = 4
+local slotCobblestone = 5
+local slotWall = 6
+local slotFloppy1 = 7
+local slotFloppy2 = 8
+local slotFloppy3 = 9
+local slotFloppy4 = 10
+local slotLight = 11
+local slotTorch = 12
+
+
+print("  _____      _  ____   _____ ")
+print(" |  __ \\    | |/ __ \\ / ____|")
+print(" | |__) |___| | |  | | (___  ")
+print(" |  _  // _ \\ | |  | |\\___ \\ ")
+print(" | | \\ \\  __/ | |__| |____) |")
+print(" |_|  \\_\\___|_|\\____/|_____/ ")
+print()
+
+
+header()
+
+print("Please fill slot " .. slotFuel .. " with Fuel")
+while(turtle.getItemCount(slotFuel) == 0) do
+	os.sleep( 0.1 )
 end
-if type(Zpos) ~= "number" then
-  print("z must be number")
-  return
+header()
+
+print("Please fill slot " .. slotComputer .. " with 4 Computers")
+while(turtle.getItemCount(slotComputer) < 4) do
+	os.sleep( 0.1 )
 end
-if type(facing) ~= "number" then
-  print("facing must be number")
-  return
+header()
+
+print("Please fill slot " .. slotDiskDrive .. " with 4 Disk Drives")
+while(turtle.getItemCount(slotDiskDrive) < 4) do
+	os.sleep( 0.1 )
 end
-print("Please fill up following slots")
-print("1 = Fuel")
-print("2 = Computer (4x)")
-print("3 = Disk drive (4x)")
-print("4 = Modem (4x)")
-print("5 - 8 = Floppy disk (4x)")
-print("Press any key to continue")
-while true do
-  event, param1 = os.pullEvent()
-  if event == "char" then
-    break
-  end
+header()
+
+print("Please fill slot " .. slotModem .. " with 4 Modems")
+while(turtle.getItemCount(slotModem) < 4) do
+	os.sleep( 0.1 )
 end
-while turtle.getItemCount(1) > 0 do
-  turtle.refuel()
+header()
+
+print("Please fill slot " .. slotCobblestone .. " with 4 Cobblestone")
+while(turtle.getItemCount(slotCobblestone) < 4) do
+	os.sleep( 0.1 )
 end
-print("Fuel level is "..turtle.getFuelLevel())
-print("Going up to 100 meter to build satelite")
-for height = 1, 100, 1 do
-  turtle.up()
+header()
+
+print("Please fill slot " .. slotWall .. " with 34 walls")
+while(turtle.getItemCount(slotWall) < 34) do
+	os.sleep( 0.1 )
 end
-Ypos = Ypos + 101
--- Go 10 forward to set the first computer
-for c = 1, 10, 1 do
-  turtle.forward()
+header()
+
+print("Please fill slot " .. slotFloppy1 .. " with a floppy")
+while(turtle.getItemCount(slotFloppy1) < 1) do
+	os.sleep( 0.1 )
 end
-if facing == 0 then
-  Zpos = Zpos + 11
+header()
+
+print("Please fill slot " .. slotFloppy2 .. " with a floppy")
+while(turtle.getItemCount(slotFloppy2) < 1) do
+	os.sleep( 0.1 )
 end
-if facing == 2 then
-  Zpos = Zpos - 11
+header()
+
+print("Please fill slot " .. slotFloppy3 .. " with a floppy")
+while(turtle.getItemCount(slotFloppy3) < 1) do
+	os.sleep( 0.1 )
 end
-if facing == 1 then
-  Xpos = Xpos - 11
+header()
+
+print("Please fill slot " .. slotFloppy4 .. " with a floppy")
+while(turtle.getItemCount(slotFloppy4) < 1) do
+	os.sleep( 0.1 )
 end
-if facing == 3 then
-  Xpos = Xpos + 11
+header()
+
+print("Please fill slot " .. slotLight .. " with 4 Redstone Lamps")
+while(turtle.getItemCount(slotLight) < 4) do
+	os.sleep( 0.1 )
 end
-buildUp(5)
--- Go back 20 and put the second computer
-turtle.turnRight()
-turtle.turnRight()
-if facing == 0 then
-  facing = 2
-elseif facing == 1 then
-  facing = 3
-elseif facing == 2 then
-  facing = 0
-elseif facing == 3 then
-  facing = 1
+header()
+
+print("Please fill slot " .. slotTorch .. " with 8 Torchs")
+while(turtle.getItemCount(slotTorch) < 8) do
+	os.sleep( 0.1 )
 end
-for c = 1, 20, 1 do
-  turtle.forward()
+header()
+
+print("All Items Acounted For!")
+
+-- Enter posisioning phase
+local posX, posY, posZ, posF = common.getTurtlePos( headerText )
+
+sm.setCoords(posX, posY, posZ, posF)
+
+-- Enter confermation phase
+header()
+print("Are you sure your ready to build (y/n)?")
+
+kill = false
+waitingConfermation = true
+
+kill = (common.standardConfermaion() == false)
+--[[
+while waitingConfermation == true do
+	event, param1, param2 = os.pullEvent("char")
+	if param1 == "y" then
+		waitingConfermation = false
+	elseif param1 == "n" then
+		waitingConfermation = false
+		kill = true
+	end
 end
-if facing == 0 then
-  Zpos = Zpos + 22
+]]
+
+
+function buildSatellite()
+
+	local startPosX, startPosY, startPosZ, startPosF = sm.getX(), sm.getY(), sm.getZ(), sm.getDir()
+
+	--sm.moveTo(startPosX - 6, startPosY, startPosZ, 3)
+
+	for i = 0,10 do
+		sm.moveTo(startPosX - 5 + i, startPosY+1, startPosZ, 3)
+		turtle.select(slotWall)
+		turtle.placeDown()
+	end
+	-- Place first node
+	-- Place Computer
+	sm.moveTo(startPosX + 6, startPosY, startPosZ, 3)
+	t.select(slotComputer)
+	t.place()
+	-- Place DiskDrive
+	sm.moveTo(startPosX + 6, startPosY-1, startPosZ, 3)
+	t.select(slotDiskDrive)
+	t.place()
+	-- Place Cobblestone Block
+	sm.moveTo(startPosX + 6, startPosY+1, startPosZ, 3)
+	t.select(slotCobblestone)
+	t.placeDown()
+	-- Place Torch on cobble Block
+	sm.moveTo(startPosX + 6, startPosY+1, startPosZ + 1, 3)
+	t.select(slotTorch)
+	t.placeDown()
+	-- Place Torch on cobble Block
+	sm.moveTo(startPosX + 6, startPosY+1, startPosZ - 1, 3)
+	t.select(slotTorch)
+	t.placeDown()
+	-- Place Modem
+	sm.moveTo(startPosX + 7, startPosY+2, startPosZ, 3)
+	t.select(slotModem)
+	t.placeDown()
+	-- Place Redstone Lamp
+	sm.moveTo(startPosX + 8, startPosY, startPosZ+1, 2)
+	t.select(slotLight)
+	t.place()
+
+	-- Turn on computer
+	sm.moveTo(startPosX + 7, startPosY, startPosZ+1, 2)
+	computer = peripheral.wrap("front")
+	sleep(0.5)
+	if computer then
+		computer.turnOn()
+	end
+
+
+	-- Place second node
+	-- Place Computer
+	sm.moveTo(startPosX - 6, startPosY, startPosZ, 1)
+	t.select(slotComputer)
+	t.place()
+	-- Place DiskDrive
+	sm.moveTo(startPosX - 6, startPosY-1, startPosZ, 1)
+	t.select(slotDiskDrive)
+	t.place()
+	-- Place Cobblestone Block
+	sm.moveTo(startPosX - 6, startPosY+1, startPosZ, 1)
+	t.select(slotCobblestone)
+	t.placeDown()
+	-- Place Torch on cobble Block
+	sm.moveTo(startPosX - 6, startPosY+1, startPosZ + 1, 1)
+	t.select(slotTorch)
+	t.placeDown()
+	-- Place Torch on cobble Block
+	sm.moveTo(startPosX - 6, startPosY+1, startPosZ - 1, 1)
+	t.select(slotTorch)
+	t.placeDown()
+	-- Place Modem
+	sm.moveTo(startPosX - 7, startPosY+2, startPosZ, 1)
+	t.select(slotModem)
+	t.placeDown()
+	-- Place Redstone Lamp
+	sm.moveTo(startPosX - 8, startPosY, startPosZ+1, 2)
+	t.select(slotLight)
+	t.place()
+
+	-- Turn on computer
+	sm.moveTo(startPosX - 7, startPosY, startPosZ+1, 2)
+	computer = peripheral.wrap("front")
+	sleep(0.5)
+	if computer then
+		computer.turnOn()
+	end
+
+	-- Place Spire
+	for i = 1,7 do
+		sm.moveTo(startPosX, startPosY+i, startPosZ, 3)
+		turtle.select(slotWall)
+		turtle.placeDown()
+	end
+
+	-- Place Top Bar
+	for i = 0,10 do
+		sm.moveTo(startPosX, startPosY+1+7, startPosZ - 5 + i, 0)
+		turtle.select(slotWall)
+		turtle.placeDown()
+	end
+
+	-- Place third node
+	-- Place Computer
+	sm.moveTo(startPosX, startPosY+7, startPosZ + 6, 0)
+	t.select(slotComputer)
+	t.place()
+	-- Place DiskDrive
+	sm.moveTo(startPosX, startPosY+7-1, startPosZ + 6, 0)
+	t.select(slotDiskDrive)
+	t.place()
+	-- Place Cobblestone Block
+	sm.moveTo(startPosX, startPosY+7+1, startPosZ + 6, 0)
+	t.select(slotCobblestone)
+	t.placeDown()
+	-- Place Torch on cobble Block
+	sm.moveTo(startPosX + 1, startPosY+7+1, startPosZ + 6, 0)
+	t.select(slotTorch)
+	t.placeDown()
+	-- Place Torch on cobble Block
+	sm.moveTo(startPosX - 1, startPosY+7+1, startPosZ + 6, 0)
+	t.select(slotTorch)
+	t.placeDown()
+	-- Place Modem
+	sm.moveTo(startPosX, startPosY+7+2, startPosZ + 7, 0)
+	t.select(slotModem)
+	t.placeDown()
+	-- Place Redstone Lamp
+	sm.moveTo(startPosX+1, startPosY+7, startPosZ + 8, 1)
+	t.select(slotLight)
+	t.place()
+
+	-- Turn on computer
+	sm.moveTo(startPosX+1, startPosY+7, startPosZ + 7, 1)
+	computer = peripheral.wrap("front")
+	sleep(0.5)
+	if computer then
+		computer.turnOn()
+	end
+
+
+	-- Place forth node
+	-- Place Computer
+	sm.moveTo(startPosX, startPosY+7, startPosZ - 6, 2)
+	t.select(slotComputer)
+	t.place()
+	-- Place DiskDrive
+	sm.moveTo(startPosX, startPosY+7-1, startPosZ - 6, 2)
+	t.select(slotDiskDrive)
+	t.place()
+	-- Place Cobblestone Block
+	sm.moveTo(startPosX, startPosY+7+1, startPosZ - 6, 2)
+	t.select(slotCobblestone)
+	t.placeDown()
+	-- Place Torch on cobble Block
+	sm.moveTo(startPosX + 1, startPosY+7+1, startPosZ - 6, 0)
+	t.select(slotTorch)
+	t.placeDown()
+	-- Place Torch on cobble Block
+	sm.moveTo(startPosX - 1, startPosY+7+1, startPosZ - 6, 0)
+	t.select(slotTorch)
+	t.placeDown()
+	-- Place Modem
+	sm.moveTo(startPosX, startPosY+7+2, startPosZ - 7, 2)
+	t.select(slotModem)
+	t.placeDown()
+	-- Place Redstone Lamp
+	sm.moveTo(startPosX+1, startPosY+7, startPosZ - 8, 1)
+	t.select(slotLight)
+	t.place()
+
+	-- Turn on computer
+	sm.moveTo(startPosX+1, startPosY+7, startPosZ - 7, 1)
+	computer = peripheral.wrap("front")
+	sleep(0.5)
+	if computer then
+		computer.turnOn()
+	end
+
+
+	-- Move to Clear Location
+	sm.moveTo(startPosX + 2, startPosY-1, startPosZ+2, 1)
 end
-if facing == 2 then
-  Zpos = Zpos - 22
-end
-if facing == 1 then
-  Xpos = Xpos - 22
-end
-if facing == 3 then
-  Xpos = Xpos + 22
-end
-buildUp(6)
--- go 10 forward to the middle
-turtle.turnRight()
-turtle.turnRight()
-if facing == 0 then
-  facing = 2
-elseif facing == 1 then
-  facing = 3
-elseif facing == 2 then
-  facing = 0
-elseif facing == 3 then
-  facing = 1
-end
-for c = 1, 10, 1 do
-  turtle.forward()
-end
-if facing == 0 then
-  Zpos = Zpos + 11
-end
-if facing == 2 then
-  Zpos = Zpos - 11
-end
-if facing == 1 then
-  Xpos = Xpos - 11
-end
-if facing == 3 then
-  Xpos = Xpos + 11
-end
--- and 10 more up
-for c = 1, 10, 1 do
-  turtle.up()
-end
-Ypos = Ypos + 10
--- turn 90 degree to setup the second row
-turtle.turnRight()
-facing = facing + 1
-if facing == 4 then
-  facing = 0
-end
--- Go 10 forward to set the third computer
-for c = 1, 10, 1 do
-  turtle.forward()
-end
-if facing == 0 then
-  Zpos = Zpos + 11
-end
-if facing == 2 then
-  Zpos = Zpos - 11
-end
-if facing == 1 then
-  Xpos = Xpos - 11
-end
-if facing == 3 then
-  Xpos = Xpos + 11
-end
-buildUp(7)
--- Go back 20 and put the last computer
-turtle.turnRight()
-turtle.turnRight()
-if facing == 0 then
-  facing = 2
-elseif facing == 1 then
-  facing = 3
-elseif facing == 2 then
-  facing = 0
-elseif facing == 3 then
-  facing = 1
-end
-for c = 1, 20, 1 do
-  turtle.forward()
-end
-if facing == 0 then
-  Zpos = Zpos + 22
-end
-if facing == 2 then
-  Zpos = Zpos - 22
-end
-if facing == 1 then
-  Xpos = Xpos - 22
-end
-if facing == 3 then
-  Xpos = Xpos + 22
-end
-buildUp(8)
--- go 10 forward to the middle
-turtle.turnRight()
-turtle.turnRight()
-for c = 1, 10, 1 do
-  turtle.forward()
-end
--- and back to earth
-while not turtle.detectDown() do
-  turtle.down()
+
+if kill == true then
+	write("Exiting...\n")
+else
+	write("Running...\n")
+	--sm.up()
+	local startPosX, startPosY, startPosZ, startPosF = sm.getX(), sm.getY(), sm.getZ(), sm.getDir()
+	sm.moveTo(startPosX, startPosY+2, startPosZ, 0)
+	sleep(1)
+	sm.moveTo(startPosX, 200, startPosZ, 0)
+	buildSatellite()
+	sleep(2)
+	sm.moveTo(startPosX, startPosY+2, startPosZ, 0)
+	sm.moveTo(startPosX, startPosY, startPosZ, sm.getDir())
+	print("Done")
 end
